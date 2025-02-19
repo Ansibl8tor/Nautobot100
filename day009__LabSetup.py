0c5a206fe9b0d85d87d88f7cf80ff9cc7eeea464
Tony <49593242+Ansibl8tor@users.noreply.github.com>
2025-02-19 14:38:18 -0500

    Create day009__LabSetup.py

---
+++ day009__LabSetup.py
@@ -0,0 +1,12 @@
+# First, build and import the db
+
+# Navigate to 100-../clab and upload the file from Arista
+
+# docker import cEOS64-lab-4.32.0F.tar.xz ceos:4.32.0F
+
+# sudo containerlab deploy --topo ceos-lab.clab.yml --node-filter bos-acc-01,bos-rtr-01
+
+# finally, start the nautobot server
+
+# in the server, create prefix 172.17.0.0/16 and ips 172.17.02,03 and assign to Boston devices management1 interface
+
